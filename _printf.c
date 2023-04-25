45984f284d9b8fc97c41b03f0db7728c4fa9aa7f
Mohamed Nayel <mohamed.abueleinein@gmail.com>
2023-04-24 19:27:23 -0700

    _printf

--- _printf.c
+++ _printf.c
@@ -1,50 +1,47 @@
 #include "main.h"
-/**
- * _printf - produces output according to a format
- * @format: a character string
- *
- * Return: the number of characters printed
- */
+#include<string.h>
+#include <unistd.h>
 int _printf(const char *format, ...)
 {
 	int count = 0;
 	va_list args;
-
 	va_start(args, format);
 
-	for (; *format != '\0'; format++)
-	{
-		if (*format == '%')
-		{
-			format++;
-			switch (*format)
-			{
-				case 'c':
-				{
-					int c = va_arg(args, int);
-
-					putchar(c);
-					count++;
-					break;
-				} case 's':
-				{
-					char *s = va_arg(args, char *);
-
-					fputs(s, stdout);
-					count += strlen(s);
-					break;
-				} case '%':
-				{
-					putchar('%');
-					count++;
-					break;
-				}
+	while (*format != '\0') {
+	if (*format == '%') {
+           format++;
+            switch (*format) {
+                case 'c':
+                    {
+                        char c = (char)va_arg(args, int);
+                        count += write(STDOUT_FILENO, &c, sizeof(char));
+                    }
+                    break;
+                case 's':
+                    {
+                        char *str = va_arg(args, char*);
+                        count += write(STDOUT_FILENO, str, strlen(str));
+                    }
+                    break;
+                case '%':
+                    {
+                        char c = '%';
+                        count += write(STDOUT_FILENO, &c, sizeof(char));
 			}
-		} else
-		{
-			putchar(*format);
-			count++;
-		}
-	} va_end(args);
-	return (count);
+                    break;
+                default:
+                    {
+                        char *msg = "Invalid format specifier";
+                        count += write(STDOUT_FILENO, msg, strlen(msg));
+                    }
+                    break;
+            }
+        } else {
+            count += write(STDOUT_FILENO, format, sizeof(char));
+        }
+        format++;
+    }
+
+    va_end(args);
+    return count;
 }
