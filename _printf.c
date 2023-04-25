ee21d778b33e536ae9a55dbb42785fb15f9ae352
mnayel09 <mohamed.abueleinein@gmail.com>
2023-04-25 05:59:26 +0200

    f

--- _printf.c
+++ _printf.c
@@ -1,47 +1,56 @@
 #include "main.h"
 #include<string.h>
 #include <unistd.h>
+/**
+ * _printf - produces the output according to format
+ * @format: a charcter string
+ *
+ * Return: the number of characters printed
+ */
 int _printf(const char *format, ...)
 {
 	int count = 0;
 	va_list args;
+
 	va_start(args, format);
+	if (*format == NULL)
+		return (-1);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					{
+						char c = (char)va_arg(args, int);
 
-	while (*format != '\0') {
-	if (*format == '%') {
-           format++;
-            switch (*format) {
-                case 'c':
-                    {
-                        char c = (char)va_arg(args, int);
-                        count += write(STDOUT_FILENO, &c, sizeof(char));
-                    }
-                    break;
-                case 's':
-                    {
-                        char *str = va_arg(args, char*);
-                        count += write(STDOUT_FILENO, str, strlen(str));
-                    }
-                    break;
-                case '%':
-                    {
-                        char c = '%';
-                        count += write(STDOUT_FILENO, &c, sizeof(char));
+						count += write(STDOUT_FILENO, &c, sizeof(char));
+					} break;
+				case 's':
+					{
+						char *str = va_arg(args, char*);
+
+						count += write(STDOUT_FILENO, str, strlen(str));
+					} break;
+				case '%':
+					{
+						char c = '%';
+
+						count += write(STDOUT_FILENO, &c, sizeof(char));
+					} break;
+				default:
+					{
+						char *msg = "Invalid format specifier";
+
+						count += write(STDOUT_FILENO, msg, strlen(msg));
+					} break;
 			}
-                    break;
-                default:
-                    {
-                        char *msg = "Invalid format specifier";
-                        count += write(STDOUT_FILENO, msg, strlen(msg));
-                    }
-                    break;
-            }
-        } else {
-            count += write(STDOUT_FILENO, format, sizeof(char));
-        }
-        format++;
-    }
-
-    va_end(args);
-    return count;
+		} else
+		{
+			count += write(STDOUT_FILENO, format, sizeof(char));
+		} format++;
+	} va_end(args);
+	return (count);
 }
