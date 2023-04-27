b8e6a679e533f1bd82d0b7ea28d15fac7c634ae3
Mohamed Nayel <mohamed.abueleinein@gmail.com>
2023-04-26 21:55:42 -0700

    _Printf

--- _printf.c
+++ _printf.c
@@ -1,47 +1,70 @@
 #include "main.h"
-#include<string.h>
 #include <unistd.h>
-int _printf(const char *format, ...)
+#include <string.h>
+/**
+ * _vprintf - produces the output count according to format
+ * @format: a charcter string
+ * @args: the format specifier
+ *
+ * Return: the number of characters printed
+ */
+int _vprintf(const char *format, va_list args)
 {
 	int count = 0;
-	va_list args;
-	va_start(args, format);
 
-	while (*format != '\0') {
-	if (*format == '%') {
-           format++;
-            switch (*format) {
-                case 'c':
-                    {
-                        char c = (char)va_arg(args, int);
-                        count += write(STDOUT_FILENO, &c, sizeof(char));
-                    }
-                    break;
-                case 's':
-                    {
-                        char *str = va_arg(args, char*);
-                        count += write(STDOUT_FILENO, str, strlen(str));
-                    }
-                    break;
-                case '%':
-                    {
-                        char c = '%';
-                        count += write(STDOUT_FILENO, &c, sizeof(char));
+	if (format == NULL)
+		return (-1);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{	case 'c':
+					{
+						char c = (char)va_arg(args, int);
+
+						count += write(STDOUT_FILENO, &c, sizeof(char));
+					} break;
+				case 's':
+					{
+						char *str = va_arg(args, char*);
+
+						count += write(STDOUT_FILENO, str, strlen(str));
+					} break;
+				case '%':
+					{
+						char c = '%';
+
+						count += write(STDOUT_FILENO, &c, sizeof(char));
+					} break;
+				default:
+					{
+						char *msg = "Invalid format specifier";
+
+						count += write(STDOUT_FILENO, msg, strlen(msg));
+					} break;
 			}
-                    break;
-                default:
-                    {
-                        char *msg = "Invalid format specifier";
-                        count += write(STDOUT_FILENO, msg, strlen(msg));
-                    }
-                    break;
-            }
-        } else {
-            count += write(STDOUT_FILENO, format, sizeof(char));
-        }
-        format++;
-    }
-
-    va_end(args);
-    return count;
+		} else
+		{
+			count += write(STDOUT_FILENO, format, sizeof(char));
+		} format++;
+	} return (count);
 }
+/**
+ * _printf - produces the output according to format
+ * @format: a charcter string
+ *
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int count;
+	va_list args;
+
+	va_start(args, format);
+	count = _vprintf(format, args);
+	va_end(args);
+	return (count);
+}
+
